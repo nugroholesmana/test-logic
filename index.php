0e9639c1a0bf45f98e83582d97322c8cdd67b357
Nugroho Lesmana Putra <nugroholesmanaputra@Nugrohos-MacBook-Pro.local>
2022-03-30 09:45:29 +0700

    tes - 1

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "hello world";
+$string= "";
+for($i=1;$i<=50;$i++){
+    if($i%3==0 && $i%5!=0){
+        $string .= "Frontend,";
+    }elseif($i%5==0 && $i%3 != 0){
+        $string .= "Backend,";
+    }elseif($i%3==0 && $i%5==0){
+        $string .= "Frontend Backend,";
+    }else{
+        $string .= $i.",";
+    }
+}
+
+$string = substr($string,0,-1);
+echo $string;
